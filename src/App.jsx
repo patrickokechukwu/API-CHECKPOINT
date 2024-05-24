200c6a74fb7886d1fd4e6e254df723a6f951b64a
patrickokechukwu <pokechukwu702@gmail.com>
2024-05-24 11:30:34 +0100

    added userlist

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Code for the App component goes here
+import './App.css';
+import {UserList} from './UserList';
+function App() {
+ 
+  return (
+    <>
+     <div className="App">
+      <header className="App-header">
+        <UserList />
+      </header>
+    </div>  
+    </>
+  );
+}
+
+export default App
+
+
